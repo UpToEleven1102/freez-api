22f808af6a4d088fe7baf084ccd5b0cf2f4b27f0
huyen vu <huyen.vu@nextgencode.io>
2019-03-14 21:57:58 -0500

    push code inside deploy script

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -5,5 +5,5 @@ dev_server=$DEV_SERVER
 environment=$ENV
 
 echo "Deploy to dev server: ${dev_server}"
-scp -r /go/src/git.nextgencode.io/huyen.vu/freez-app-rest/ ubuntu@35.162.158.187:/home/ubuntu/go/src/git.nextgencode.io/huyen.vu
+scp -r $CI_PROJECT_DIR ubuntu@35.162.158.187:/home/ubuntu/go/src/git.nextgencode.io/huyen.vu
 ssh ubuntu@${dev_server} "cd ~/go/src/git.nextgencode.io/huyen.vu/freez-app-rest && sudo docker-compose up"
