56bb7839212b02b6999f862ee9b377b742ab0d80
huyen vu <huyen.vu@nextgencode.io>
2019-03-14 22:37:18 -0500

    should work, ec2's disk is full

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -6,4 +6,4 @@ environment=$ENV
 
 echo "Deploy to dev server: ${dev_server}"
 scp -r $CI_PROJECT_DIR ubuntu@35.162.158.187:/home/ubuntu/go/src/git.nextgencode.io/huyen.vu
-ssh ubuntu@${dev_server} "cd ~/go/src/git.nextgencode.io/huyen.vu/freez-app-rest && sudo docker-compose kill && sudo docker-compose rm -v && sudo docker build -t freez-app-rest . && sudo docker-compose up"
+ssh ubuntu@${dev_server} "cd ~/go/src/git.nextgencode.io/huyen.vu/freez-app-rest && sudo docker-compose kill && sudo docker-compose rm -v && sudo docker build -t freez-app-rest . && sudo docker-compose up -d"
