18e4f09f234f336c065ef266f15045d4a98ca557
huyen vu <huyen.vu@nextgencode.io>
2019-03-14 21:44:17 -0500

    change deploy script

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -6,4 +6,4 @@ environment=$ENV
 
 echo "Deploy to dev server: ${dev_server}"
 make push-code-gitlab
-ssh ubuntu@${dev_server} "cd ~/go/src/git.nextgencode.io/huyen.vu/freez-app-rest && sudo docker-compose up"
+ssh -i ~/.ssh/id_rsa ubuntu@${dev_server} "cd ~/go/src/git.nextgencode.io/huyen.vu/freez-app-rest && sudo docker-compose up"
