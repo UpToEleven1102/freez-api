fd656e396e9f804ed8b5722bfe95beaf58efbf56
huyen vu <huyen.vu@nextgencode.io>
2019-03-23 17:47:52 -0500

    change deploy script to master branch

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -6,4 +6,4 @@ environment=$ENV
 
 echo "Deploy to dev server: ${dev_server}"
 echo "Building exe file"
-ssh ubuntu@${dev_server} "cd ~/go/src/git.nextgencode.io/huyen.vu/freez-app-rest && git checkout . && git pull origin develop && sudo docker-compose down && sudo docker-compose rm -v && PATH=$PATH:/usr/local/go/bin && make build && sudo docker build -t freez-app-rest . && sudo docker-compose up -d"
+ssh ubuntu@${dev_server} "cd ~/go/src/git.nextgencode.io/huyen.vu/freez-app-rest && git checkout . && git pull origin master && sudo docker-compose down && sudo docker-compose rm -v && PATH=$PATH:/usr/local/go/bin && make build && sudo docker build -t freez-app-rest . && sudo docker-compose up -d"
